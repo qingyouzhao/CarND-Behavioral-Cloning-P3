cc2d51ed59491a436dd4144d08c9890ece7333d1
qingyouzhao <zhaoqingyou315@gmail.com>
2019-03-31 23:03:18 -0700

    Added attempt to train with imagenet inception nety

--- model.py
+++ model.py
@@ -3,10 +3,26 @@ import pandas as pd
 import cv2
 import numpy as np
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Convolution2D, MaxPooling2D
+from keras.layers import Flatten, Dense, Lambda, Convolution2D, MaxPooling2D, Cropping2D
+from keras.applications.vgg16 import VGG16
+import os
+import psutil
+
+def print_memory_usage():
+    process = psutil.Process(os.getpid())
+    print("Current process using memoery: {0} MB".format((process.memory_info().rss / 1024/1024)))  # in bytes
+
 
 def read_image(image_file):
     return cv2.imread(image_file)
+
+def flip_image(image):
+    """
+    Flip the image left to right
+    :param image:
+    :return:
+    """
+    return np.fliplr(image)
 
 def pre_process(image):
     """
@@ -28,21 +44,58 @@ def image_visualization(data_directory='/driving_data/data'):
     cv2.imshow('image', img)
     cv2.waitKey()
 
-def read_training_data(data_directory, use_head = False):
+def read_training_data(data_directory, use_head = False, num_images=-1):
     driving_log = read_driving_log(data_directory)
+    if num_images != -1:
+        driving_log = driving_log.head(num_images)
+
     if use_head:
         driving_log = driving_log.head()
+
+
+
+    # Create full driving names
     driving_log['center_full'] = driving_log.loc[:,'center'].apply(lambda x: os.path.join(data_directory, x))
-    driving_log['image'] = driving_log.loc[:,'center_full'].apply(read_image)
+    driving_log['right_full'] = driving_log.loc[:, 'right'].apply(lambda x: os.path.join(data_directory, x))
+    driving_log['left_full'] = driving_log.loc[:, 'left'].apply(lambda x: os.path.join(data_directory, x))
 
-    X_train = np.zeros([driving_log['image'].shape[0]]+list(driving_log['image'].values[0].shape))
-    y_train = np.zeros(driving_log['steering'].shape[0])
+    # read image into data frame
+    driving_log['image_center'] = driving_log.loc[:,'center_full'].apply(read_image)
+    # driving_log['image_left'] = driving_log.loc[:, 'left_full'].apply(read_image)
+    # driving_log['image_right'] = driving_log.loc[:, 'right_full'].apply(read_image)
 
-    for idx in range(driving_log['image'].shape[-1]):
-        X_train[idx] = driving_log['image'][idx]
-        y_train[idx] = driving_log['steering'][idx]
+    # augment data with flipped
+    X_train= {}
+    y_train = {}
+    X_train['center'] = np.zeros([driving_log['image_center'].shape[0]]+list(driving_log['image_center'].values[0].shape))
+    y_train['center'] = np.zeros(driving_log['image_center'].shape[0])
+    X_train['center_flipped'] = np.zeros([driving_log['image_center'].shape[0]]+list(driving_log['image_center'].values[0].shape))
+    y_train['center_flipped'] = np.zeros(driving_log['image_center'].shape[0])
 
-    return X_train, y_train
+    print("Center has shape {0}".format(X_train['center'].shape))
+    print("Center flipped has shape {0}".format(X_train['center_flipped'].shape))
+
+
+    for idx in range(driving_log['image_center'].shape[-1]):
+        if(idx%500==0):
+            print_memory_usage()
+        X_train['center'][idx] = driving_log['image_center'][idx]
+        y_train['center'][idx] = driving_log['steering'][idx]
+        X_train['center_flipped'][idx] = flip_image(driving_log['image_center'][idx])
+        y_train['center_flipped'][idx] = -driving_log['steering'][idx]
+
+    # create training data by appending needed arrays
+    print("Center has shape {0}".format(X_train['center'].shape))
+    print("Center flipped has shape {0}".format(X_train['center_flipped'].shape))
+
+    # todo(qingyouz): I need to know more about the difference between
+    # https://docs.scipy.org/doc/numpy/reference/generated/numpy.append.html
+    # https://docs.scipy.org/doc/numpy/reference/generated/numpy.concatenate.html
+    # but forenow, let's just use np.append
+    X_train_combined = np.append(X_train['center'], X_train['center_flipped'], axis=0)
+    y_train_combined = np.append(y_train['center'], y_train['center_flipped'], axis=0)
+
+    return X_train_combined, y_train_combined
 
 
 
@@ -63,7 +116,6 @@ def LeNet_model(X_train, y_train, save_path='model.h5', epochs=5):
 
     input_shape = X_train[0].shape
     print(input_shape)
-    print(y_train[:200])
     model = Sequential()
 
     model.add(Lambda(lambda x: (x/255.0)-0.5, input_shape=input_shape))
@@ -83,14 +135,61 @@ def LeNet_model(X_train, y_train, save_path='model.h5', epochs=5):
 
     model.save(save_path)
 
+def add_pre_process_layers(model, input_shape):
+    """
+    Shared utility to add preprocess model.
+    :param model: the Sequentiao model
+    :param input_shape:
+    :return: the model with pre-process layers
+    """
+
+    model.add(Cropping2D(cropping=((50, 20), (0, 0)), input_shape=input_shape))
+    model.add(Lambda(lambda x: (x / 255.0) - 0.5))
+
+    return model
+
+# Transfer learning models
+
+
+# training models from scratch:
+from keras.applications.inception_v3 import InceptionV3
+from keras.models import Model
+from keras import backend as K
+
+def pre_trained_inception(X_train, y_train, save_path='InceptionV3.h5', epochs=5):
+    # create the base pre-trained model
+    input_shape = X_train[0].shape
+    print(input_shape)
+
+
+    base_model = InceptionV3(weights='imagenet', include_top=False, input_shape=input_shape)
+
+    x = Flatten()(base_model.output)
+    x = Dense(1024)(x)
+    x = Dense(200)(x)
+    x = Dense(1)(x)
+
+    # this is the model we will train
+    model = Model(inputs=base_model.input, outputs=x)
+
+    # make the inception model part not trainable
+    for layer in base_model.layers:
+        layer.trainable = False
+
+    print(model.summary())
+
+    model.compile(loss='mse',optimizer='adam')
+    model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=epochs)
+
+    model.save(save_path)
 
 def main():
     data_directory = os.path.join('driving_data','data')
-    X_train, y_train = read_training_data(data_directory, use_head=False)
+    X_train, y_train = read_training_data(data_directory, use_head=False,num_images=1000)
     # used to test pipeline
     # image_test_model(X_train,y_train)
 
-    LeNet_model(X_train,y_train)
+    pre_trained_inception(X_train,y_train)
 
 if __name__ == '__main__':
     main()
